fb5a6941f561307535db49b9a9abf8f50c874167
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-11-26 02:43:09 +0200

    DB Connection

--- server/server.js
+++ server/server.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
+const connectDB = require("./config/db");
+const colors = require("colors");
+const morgan = require("morgan");
+
 const chats = require("./data/data");
+
+connectDB();
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(logger);
+if ((process.env.NODE_ENV = "development")) {
+  app.use(morgan("dev"));
+}
 
 app.get("/", (req, res) => {
   res.send("API is Running Successfully");
@@ -18,4 +33,4 @@ app.get("/api/chat/:id", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`server running on port ${PORT}`));
+app.listen(PORT, console.log(`server running on port ${PORT}`.yellow.bold));
